8e7d5218e211cbd3a12b0f578df18a57b1ca2b3e
Gustavo Castro <work@gvcastro.com>
2025-03-31 01:03:51 -0300

    Initial commmit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Math Utils
+
+A collection of math programs that (hopefully) will eliminate my need to look for online calculators.
---
+++ grp_ltx.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import math
+import argparse
+
+"""
+Almost here means all divisors other than 1
+"""
+
+
+def get_almost_all_divisors(x: int) -> set:
+    s = set()
+    for i in range(2, x + 1):
+        if x % i == 0:
+            s.add(i)
+    return s
+
+
+"""
+grp_lt means get relative primes less than x. x is the func. parameter
+"""
+
+
+def grp_lx(x: int) -> set | None:
+    s = set()
+    d = get_almost_all_divisors(x)
+    for i in range(2, math.floor(x / 2) + 1):
+        if get_almost_all_divisors(i).isdisjoint(d):
+            s.add(i)
+
+    return None if (not s) else s
+
+
+def main(x: int):
+    print(f"{x}:\t{grp_lx(x)}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Find all relative primes up to x")
+    parser.add_argument(
+        "-x",
+        "--x",
+        type=int,
+        required=True,
+        help="Number up to which the relative primes will be found",
+    )
+    args = parser.parse_args()
+    x = args.x
+
+    main(x)
